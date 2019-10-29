f6d6972bd1a534a85399949ee8a15a738b60bd0b
xie926 <1187904163@qq.com>
2019-10-29 14:27:32 +0800

    car

---
+++ wxapp/car/pages/stories/index.js
@@ -0,0 +1,75 @@
+// pages/stories/index.js
+const db = require("../../assets/db.js");
+let currentVideo = null;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    stories: db.stories,
+    currentVid: null
+  },
+  play:function(event) {
+    const vid = event.target.dataset.id;
+    if(vid) {
+      wx.createVideoContext(vid);
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
