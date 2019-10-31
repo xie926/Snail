7475a16bcb7cc3f166e7a27fd6e9ee210c90c5be
xie926 <1187904163@qq.com>
2019-10-31 16:23:19 +0800

    shopping

---
+++ wxapp/shopping/pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+const app = getApp();
+
+Page({
+  data: {
+    imgUrls: [
+      '/image/b1.jpg',
+      '/image/b2.jpg',
+      '/image/b3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 800
+  }
+})
