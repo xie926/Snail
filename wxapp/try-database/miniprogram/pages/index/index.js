68e93c981bd265b058009d73e61fb791ae715e0a
xie926 <1187904163@qq.com>
2019-11-01 23:44:52 +0800

    try-database

---
+++ wxapp/try-database/miniprogram/pages/index/index.js
@@ -0,0 +1,52 @@
+//index.js
+const app = getApp()
+const db = wx.cloud.database();
+const productsCollection =
+  db.collection('products');
+
+Page({
+  data: {
+    products: [],
+    page: 0
+  },
+  onLoad() {
+    //  lifecycle
+    productsCollection
+      .get()
+      .then(res => {
+        // console.log(res.data);
+        this.setData({
+          products: res.data
+        })
+      })
+  },
+  onPullDownRefresh: function() {
+    productsCollection
+    .get()
+    .then(res => {
+      this.setData({
+        page: 0,
+        products: res.data
+      }, res => {
+        wx.stopPullDownRefresh();
+      })
+    })
+  },
+  onReachBottom: function () {
+    wx.showLoading();
+    let page = this.data.page + 14;
+    // let old_data = this.data.products;
+    productsCollection
+    .skip(page)
+    .get()
+    .then(res => {
+      wx.hideLoading();
+      let new_data = res.data;
+      let old_data = this.data.products;
+      this.setData({
+        products: old_data.concat(new_data),
+        page:page
+      })
+    })
+  }
+})
