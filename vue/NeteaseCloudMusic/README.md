e547935948ec5a59a3998a281c4bfa9461a32dcb
xie926 <1187904163@qq.com>
2019-11-08 19:25:44 +0800

    NeteaseCloudMusic

---
+++ vue/NeteaseCloudMusic/README.md
@@ -0,0 +1,10 @@
+# vue-music 项目依赖
+1. npm install --save fastclick         解决掉ios手机页面上点击页面会有300ms的延时来判断来那个操作是否执行双击操作的依赖
+2. npm install vue-lazyload --save      实现图片的懒加载依赖
+3. npm install axios --save -dev        axios依赖
+4. npm install stylus --save-dev        安装stylus
+5. npm install stylus-loader --save-dev 安装stylus-loader
+6. npm install vuex                     安装vuex
+7. npm install better-scroll --save     安装better-scroll
+## flex参数
+  flex 0 0 px2rem(88px) === width px2rem(88px)
---
+++ vue/NeteaseCloudMusic/src/pages/search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <v-search @query="onQueryChange" ref="searchBox"></v-search> <!-- ref重要 -->
+    </div>
+    <div class="shortcut-wrapper" ref="shortcutWrapper" v-show="!query"> <!-- vuex中取出的query -->
+      <!-- 热门搜索 -->
+      <v-scroll>
+        <div class="item">10</div>
+      </v-scroll>
+    </div>
+  </div>
+</template>
+
+<script>
+import searchBox from '@/components/searchBox'
+import scroll from '@/components/scroll'
+export default {
+  data () {
+    return {
+      query: ''
+    }
+  },
+  components: {
+    'v-search': searchBox,
+    'v-scroll': scroll
+  },
+  methods: {
+    onQueryChange () {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item
+  height 80px
+</style>
